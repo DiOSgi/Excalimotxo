b8d1a204d1692e0baabb6f0247de447ba06897b6
melero-benat <benatmele@gmail.com>
2026-05-17 12:47:15 +0200

    animaciones

--- excali/espata.gd
+++ excali/espata.gd
@@ -2,17 +2,28 @@ extends Node2D
 
 
 func _ready() -> void:
-	$AnimationPlayer.play("idle")
+	update_animations("idle")
 
 
 
 func _process(delta: float) -> void:
-	update_animations("idle")
+	pass
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("ATACAR"):
-		if Guia.atacando == true:
-			update_animations("atack")
+		if Guia.attack == 1:
+			update_animations("attack")
+			await get_tree().create_timer(0.5).timeout
+			update_animations("idle")
+		else:
+			update_animations("idle")
+
 
 func update_animations(animation):
+	if $AnimationPlayer.current_animation == animation:
+		return
 	$AnimationPlayer.play(animation)
+
+func on_animation_finished(anim_name: String) -> void:
+	if anim_name == "attack":
+		update_animations("idle")
