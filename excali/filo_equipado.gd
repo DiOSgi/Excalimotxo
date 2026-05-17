ba40fdc718b93b06fce42ba67c877056212a2b0e
Diogiii <julenriveragarcia@gmail.com>
2026-05-17 13:09:20 +0200

    cum de mono

---
+++ excali/filo_equipado.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+var velocidad : int
+var spawnrate : float
+var tamañobarra
+var daño : int
+var parry : float
+var freezetimebarra #Provisional, testear, puede ir anexado a la velocidad
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	velocidad = 2
+	spawnrate = 2
+	tamañobarra = 3
+	daño = 3
+	parry = 2
+	# Si terminamos poniendo tipos de filos, seria hacer otra variable
+	# llamada "tipo" que diga si es espada, mandoble, daga...
+	#entonces seria poner las variables iniciales dentro de ifs de si es espada o no y etc.
+
+func set_estadisticas() -> void:
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
