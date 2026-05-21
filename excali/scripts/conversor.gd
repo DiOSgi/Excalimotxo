de1226f858645d8bf110f0e4dbeca2854026c31f
melero-benat <melero.benat@uni.eus>
2026-05-21 09:45:31 +0200

    .

--- excali/scripts/conversor.gd
+++ excali/scripts/conversor.gd
@@ -1,10 +1,37 @@
 extends Node2D
 
+var velocidad : int
+var spawnrate : float
+var tamañobarra
+var daño : int
+var parry : float
+var freezetimebarra #Provisional, testear, puede ir anexado a la velocidad
+@onready var script_filo = load("res://scripts/filo_equipado.gd")
+@onready var script_mango = load("res://scripts/mango_equipado.gd")
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
-	pass # Replace with function body.
+	if script_filo and script_mango:
+		var filo = script_filo.new()
+		var mango = script_mango.new()
+		
+		velocidad = (filo.velocidad + mango.velocidad) * 2
+		spawnrate = (1.5 - (filo.spawnrate + mango.spawnrate)*0.15)
+		#tamañobarra = (filo.tamañobarra + mango.tamañobarra)*5
+		daño = (filo.daño + mango.daño)*2
+		parry = (filo.parry + mango.parry)*0.2
+		
+		print("Velocidad: ", velocidad)
+		print("Spawnrate: ", spawnrate)
+		print("Tamaño Barra: ", tamañobarra)
+		print("Daño: ", daño)
+		print("Parry: ", parry)
+	# Si terminamos poniendo tipos de filos, seria hacer otra variable
+	# llamada "tipo" que diga si es espada, mandoble, daga...
+	#entonces seria poner las variables iniciales dentro de ifs de si es espada o no y etc.
 
+func set_estadisticas() -> void:
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
--- excali/scripts/mango_equipado.gd
+++ excali/scripts/mango_equipado.gd
@@ -2,17 +2,19 @@ extends Node2D
 
 var velocidad : int = 2
 var spawnrate : float = 2
-var tamañobarra 
+var tamañobarra = 3
 var daño : int = 3
 var parry : float = 2
 var freezetimebarra #Provisional, testear, puede ir anexado a la velocidad
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
-	# Si terminamos poniendo tipos de filos, seria hacer otra variable
-	# llamada "tipo" que diga si es espada, mandoble, daga...
-	#entonces seria poner las variables iniciales dentro de ifs de si es espada o no y etc.
+	velocidad = 2
+	spawnrate = 2
+	tamañobarra = 3
+	daño = 3
+	parry = 2
+
 
 func set_estadisticas() -> void:
 	pass
